5ee6dc65cc3f24e07411aab2143e54cf81b2aa86
Luiz Chun <luizch98@gmail.com>
2022-07-24 16:49:34 -0300

    Initial with Models

---
+++ src/Example.API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Example.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Example.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<People> Peoples { get; set; }
+
+
+
+    }
+}
---
+++ src/Example.API/Models/People.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Example.API.Models
+{
+    public class People
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(11)]
+        public string CPF { get; set; }
+
+        [ForeignKey("City")]
+        [Display(Name = "City name")]
+        public int ID_City { get; set; }
+
+        [Required]
+        [StringLength(3)]
+        public  int Age  { get; set; }
+
+        public People()
+        {
+        }
+
+        public People(string name, string cpf, int id_city, int age)
+        {
+            Name = name;
+            CPF = cpf;
+            ID_City = id_city;
+            Age = age;
+        }
+
+
+
+
+    }
+}
